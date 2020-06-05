3a8d3926adb31d2564cdb0db0018895bf0f67d5e
Paulo Egidio <paulo_els@hotmail.com>
2020-06-05 13:55:38 -0300

    Front-end finalizado

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+import Routes from './routes'
+
+// JSX: Permite Sintaxe de XML dentro do JavaScript
+
+function App() {
+  return ( 
+
+    <Routes />
+    
+  );
+}
+
+export default App;
+
+
+// div#app>ul>li*5
